7ca0b44c722829d2acbb306fcd152e612cd0eba0
Mayank Yadav <connectmayank@gmail.com>
2016-07-23 09:11:09 -0400

    first commit for wired

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,93 @@
+class PostsController < InheritedResources::Base
+  before_action :authenticate_user!
+  before_action :set_post, only: [:show, :edit, :update, :destroy, :upvote, :downvote]
+
+ # GET /posts
+  # GET /posts.json
+  def index
+    @posts = Post.all.order(:cached_votes_up => :desc)
+  end
+
+  # GET /post/1
+  # GET /post/1.json
+  def show
+  end
+
+  # GET /posts/new
+  def new
+    @post = Post.new
+  end
+
+  # GET /posts/1/edit
+  def edit
+  end
+
+  # POST /posts
+  # POST /posts.json
+  def create
+    @post = Post.new(post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to @post, notice: 'post was successfully created.' }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :new }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /posts/1
+  # PATCH/PUT /posts/1.json
+  def update
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to @post, notice: 'post was successfully updated.' }
+        format.json { render :show, status: :ok, location: @post }
+      else
+        format.html { render :edit }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /posts/1
+  # DELETE /posts/1.json
+  def destroy
+    @post.destroy
+    respond_to do |format|
+      format.html { redirect_to posts_url, notice: 'post was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end  
+
+
+
+
+
+
+
+  def upvote
+    	@post.upvote_from current_user
+    	redirect_to posts_path
+    end
+    
+    def downvote
+    	@post.downvote_from current_user
+    	redirect_to posts_path
+    end
+
+  private
+     # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.find(params[:id])
+    end
+    def post_params
+      params.require(:post).permit(:attachment, :content, :user_id)
+    end
+    
+#upvoate from the user
+#down vote from the user
+end
+
