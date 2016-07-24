0f8b4c8106aea7b734fbdb43ff9bcea0a608fae4
Mayank Yadav <connectmayank@gmail.com>
2016-07-24 17:46:40 -0400

    added redirect to same page in controller and show user id on the index view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,11 +27,13 @@ class PostsController < InheritedResources::Base
   # POST /posts.json
   def create
     @post = Post.new(post_params)
+    @post.user_id = current_user.id
+
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: 'post was successfully created.' }
-        format.json { render :show, status: :created, location: @post }
+        format.html { redirect_to :back, notice: 'post was successfully created.' }
+        format.json { render :index, status: :created, location: @post }
       else
         format.html { render :new }
         format.json { render json: @post.errors, status: :unprocessable_entity }
